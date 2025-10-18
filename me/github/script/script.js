afd1090303e82f5f7cc6f0cd488da3eda331425c
oliver <oliverxiaozhen@outlook.com>
2025-10-18 17:39:27 +0800

    修复注销按钮的 ID，确保与 JavaScript 中的元素匹配

--- me/github/script/script.js
+++ me/github/script/script.js
@@ -5,6 +5,28 @@ const avatar = document.getElementById("avatar");
 const Repositories = document.getElementById("Repositories");
 const Followers = document.getElementById("Followers");
 const Following = document.getElementById("Following");
+const logout_btn = document.getElementById("logout_btn");
+const Log_out_bin = document.getElementById("Log_out_bin");
+
+logout_btn.addEventListener('click',function(){
+    fetch('/api/exit.ts')
+    .then(data => {
+        if(!data.status === 'ok'){
+            alert("好像有点问题，我们好像未能给您退出登录。");
+        }
+        else{
+            window.location.href = '/';
+        }
+    })
+    .catch(error =>{
+        console.error(error);
+        alert("出现错误，我们好像未能给您退出登录。")
+    })
+})
+
+Log_out_bin.addEventListener('click',function(){
+    alert("再等等，注销授权功能正在上线")
+})
 
 
 fetch("/api/me")
